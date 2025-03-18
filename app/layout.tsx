b73734f86996a67839a931c8289cecd959fa6ba0
Gaston Arthur <gastonarthur14@gmail.com>
2025-03-18 12:28:30 -0300

    Head add

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,18 +10,20 @@ import { Analytics } from "@vercel/analytics/react";
 const montserrat = Montserrat({ subsets: ["latin"], variable: "--font-montserrat" });
 const roboto = Roboto({ subsets: ["latin"], variable: "--font-roboto" });
 
-export const metadata = {
-  title: "Tu página",
-  icons: {
-    icon: "public/favicon.ico", // Favicon principal
-    apple: "public/favicon.png", // Apple Touch Icon
-  },
-};
-
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es" suppressHydrationWarning>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>Maycam Evolve</title>
+        <meta name="description" content="Web del equipo de eSports MAYCAM EVOLVE" />
+        <meta name="author" content="GastonArthur" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
+        </head>
       <body className={`${montserrat.variable} ${roboto.variable}`}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           <div className="flex flex-col min-h-screen bg-black">
